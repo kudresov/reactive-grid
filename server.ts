20f2e22ea6cf263bdcc0f0b98829526e3bf79ef6
Vitalij Kudresov <kudresov@gmail.com>
2017-08-18 23:41:55 +0100

    deployement setup

--- server.ts
+++ server.ts
@@ -2,14 +2,16 @@ import * as express from 'express';
 import * as path from 'path';
 
 const app = express();
+const isProd = process.env.NODE_ENV === 'production';
+const clientPath = isProd ? path.join(__dirname, 'public') : __dirname;
+const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 
-app.use(express.static('dist'));
-app.use(express.static('node_modules'));
+app.use(express.static(clientPath));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(clientPath, 'index.html'));
 });
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000');
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,64 @@
 const DashboardPlugin = require('webpack-dashboard/plugin');
+const webpack = require('webpack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin;
 
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 module.exports = {
   entry: './src/client/index.tsx',
   output: {
-    filename: 'bundle.js',
-    path: __dirname + '/dist'
+    filename: '[name].[hash].js',
+    path: __dirname + '/dist/public'
   },
 
-  plugins: [new DashboardPlugin()],
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      mangle: true,
+      compress: {
+        warnings: false, // Suppress uglification warnings
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+        screw_ie8: true
+      },
+      output: {
+        comments: false
+      },
+      exclude: [/\.min\.js$/gi] // skip pre-minified libs
+    }),
+    new BundleAnalyzerPlugin({ analyzerMode: 'none', openAnalyzer: false }),
+    new CopyWebpackPlugin([{ from: './index.html', to: './index.html' }])
+  ],
 
   // Enable sourcemaps for debugging webpack's output.
   devtool: 'source-map',
 
   resolve: {
     // Add '.ts' and '.tsx' as resolvable extensions.
-    extensions: ['.ts', '.tsx', '.js', '.json']
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+    modules: ['node_modules'],
+    alias: {
+      react: 'preact-compat',
+      'react-dom': 'preact-compat'
+    }
   },
 
   module: {
     rules: [
       // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-      { test: /\.tsx?$/, loader: 'awesome-typescript-loader' },
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader',
+        exclude: /node_modules/
+      },
 
       // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
       { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' }
     ]
-  },
-
-  // When importing a module whose path matches one of the following, just
-  // assume a corresponding global variable exists and use that instead.
-  // This is important because it allows us to avoid bundling all of our
-  // dependencies, which allows browsers to cache those libraries between builds.
-  externals: {
-    react: 'React',
-    'react-dom': 'ReactDOM'
   }
 };
