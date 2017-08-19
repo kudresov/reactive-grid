ab77de1ccf0d885e78b6f5421a00fab3c5464be4
Vitalij Kudresov <kudresov@gmail.com>
2017-08-19 11:44:32 +0100

    added server push

--- server.ts
+++ server.ts
@@ -1,7 +1,12 @@
 import * as express from 'express';
 import * as path from 'path';
+import * as morgan from 'morgan';
+import * as netjet from 'netjet';
 
 const app = express();
+app.use(netjet);
+app.use(morgan);
+
 const isProd = process.env.NODE_ENV === 'production';
 const clientPath = isProd ? path.join(__dirname, 'public') : __dirname;
 const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
