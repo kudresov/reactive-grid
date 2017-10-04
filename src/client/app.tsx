ef3420408ae74e7656f8ff7240a44764c6bb3ac9
Vitalij Kudresov <kudresov@gmail.com>
2017-10-04 14:09:54 +0100

    fixed apollo

--- src/client/app.tsx
+++ src/client/app.tsx
@@ -10,8 +10,15 @@ import { createStore, applyMiddleware } from 'redux';
 import GithubLoadable from './components/github-container';
 import { ApolloClient, ApolloProvider } from 'react-apollo';
 import dynamicMiddlewares from './redux-dynamic-middlewares';
+import createReducer from './reducers';
 
-export const store = createStore(applyMiddleware(dynamicMiddlewares));
+const store = createStore(createReducer(), applyMiddleware(dynamicMiddlewares));
+(store as any).asyncReducers = {};
+
+export const injectAsyncReducer = (name, asyncReducer) => {
+  (store as any).asyncReducers[name] = asyncReducer;
+  store.replaceReducer(createReducer((store as any).asyncReducers));
+};
 
 const App = () => (
   <Provider store={store}>
--- src/client/components/github-page.tsx
+++ src/client/components/github-page.tsx
@@ -2,33 +2,32 @@ import * as React from 'react';
 import GitHubStars from './github-stars';
 import GitHubRepos from './github-repos';
 import { addMiddleware } from '../redux-dynamic-middlewares';
-import { injectAsyncReducer } from '../store';
-import { store } from '../app';
+import { injectAsyncReducer } from '../app';
 
 import {
   ApolloClient,
   gql,
   ApolloProvider,
-  createNetworkInterface
+  createNetworkInterface,
 } from 'react-apollo';
 const networkInterface = createNetworkInterface({
-  uri: '/api/github/graphql'
+  uri: '/api/github/graphql',
 });
 
 const client = new ApolloClient({
-  networkInterface
+  networkInterface,
 });
 
 networkInterface.use([
   {
     applyMiddleware(req, next) {
       next();
-    }
-  }
+    },
+  },
 ]);
 
 addMiddleware(client.middleware());
-injectAsyncReducer(store, 'zap', client.reducer());
+injectAsyncReducer('apollo', client.reducer());
 
 const GitHubPage: React.SFC = () => (
   <ApolloProvider client={client}>
