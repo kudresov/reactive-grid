5f27df213b8af02e7ab04215637a5266b6976170
Vitalij Kudresov <kudresov@gmail.com>
2017-11-17 21:42:03 +0000

    wip

--- src/client/components/github/github-section.tsx
+++ src/client/components/github/github-section.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Repo } from '../../../typings';
-import Loader from '../loader/loader';
+import DotsLoader from '../dots-loader/dots-loader';
 
 const styles = require('./github-section.css');
 
@@ -27,38 +27,36 @@ const GitHubSection: React.SFC<Props & OwnProps> = ({
   getPrevious,
   title,
   imgSrc
-}) => {
-  return (
-    <div className={styles.container}>
-      <div className={styles.headerContainer}>
-        <img className={styles.icon} src={imgSrc} />
-        <h2 className={styles.header}>{title}</h2>
-      </div>
-      <div className={styles.listContainer}>
-        {loading ? (
-          <Loader />
-        ) : (
-          <ul className={styles.list}>
-            {repos.map(n => <li key={n.name}>{n.name}</li>)}
-          </ul>
-        )}
-      </div>
-
-      <a
-        className={hasPrevious ? styles.link : styles.linkDisabled}
-        onClick={getPrevious}
-      >
-        newer
-      </a>
-      <span className={styles.separator}> | </span>
-      <a
-        className={hasNext ? styles.link : styles.linkDisabled}
-        onClick={getNext}
-      >
-        older
-      </a>
+}) => (
+  <div className={styles.container}>
+    <div className={styles.headerContainer}>
+      <img className={styles.icon} src={imgSrc} />
+      <h2 className={styles.header}>{title}</h2>
     </div>
-  );
-};
+    <div className={styles.listContainer}>
+      {loading ? (
+        <DotsLoader />
+      ) : (
+        <ul className={styles.list}>
+          {repos.map(n => <li key={n.name}>{n.name}</li>)}
+        </ul>
+      )}
+    </div>
+
+    <a
+      className={hasPrevious ? styles.link : styles.linkDisabled}
+      onClick={getPrevious}
+    >
+      newer
+    </a>
+    <span className={styles.separator}> | </span>
+    <a
+      className={hasNext ? styles.link : styles.linkDisabled}
+      onClick={getNext}
+    >
+      older
+    </a>
+  </div>
+);
 
 export default GitHubSection;
