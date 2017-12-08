4e0773480a433ed6046e03711b7ff098f664ed20
Vitalij Kudresov <kudresov@gmail.com>
2017-12-08 00:06:16 +0000

    fixed styling on blog overview on mobile

--- src/client/components/blog/angular-env-var/angular-env-var-overview.tsx
+++ src/client/components/blog/angular-env-var/angular-env-var-overview.tsx
@@ -5,7 +5,7 @@ const styles = require('../blog-item-overview.css');
 
 const AngularEnvVarOverview: SFC = () => (
   <Link className={styles.link} to="blog/a-better-way-to-inject-environmental-variables-in-angular">
-    <div>
+    <div className={styles.itemContainer}>
       <p className={styles.date}>19 Sep 2015</p>
       <h1 className={styles.title}>A better way to inject environmental variables in angular</h1>
       <h2 className={styles.summary}>Have you experience problems passing your enviromental variable from you express server to your angular client? I will go over few approaches and outline their advantages and disadvantages</h2>
--- src/client/components/blog/sample-blog/sample-blog-overview.tsx
+++ src/client/components/blog/sample-blog/sample-blog-overview.tsx
@@ -5,7 +5,7 @@ const styles = require('../blog-item-overview.css');
 
 const SampleBlogOverview: SFC = () => (
   <Link className={styles.link} to="blog/sample-blog">
-    <div>
+    <div className={styles.itemContainer}>
       <p className={styles.date}>08 Nov 2017</p>
       <h1 className={styles.title}>Sample Blog Title</h1>
       <h2 className={styles.summary}>If you are developing something which is slightly more complicated than ‘hello world’ you should have multiple environments like: develop, staging, production. Depending on the environment you would use different resource</h2>
